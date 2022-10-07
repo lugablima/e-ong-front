e1406a0f0c9c3bb7504146cff3f2a930f3489a85
Lucas <lugablima@gmail.com>
2022-10-07 16:43:35 -0300

    feat: add the map centered on the user's location on the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,50 @@
 // import { Box, Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
-// import Header from "../components/Header/Header";
-// import SideBar from "../components/SideBar/SideBar";
+function LocationMarker() {
+  const map = useMap();
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+        map.flyTo([position.coords.latitude, position.coords.longitude], 13);
+      });
+    } else {
+      alert("Sem!");
+    }
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <Marker position={[latitude, longitude]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  );
+}
 
 export default function HomePage() {
   return (
-    // <Box w="100vw" h="100vh">
-    //   <SideBar />
-    //   <Flex direction="column">
-    //     <Header />
-    //     <Text color="secondary">Mapa</Text>
-    //   </Flex>
-    // </Box>
-    <h1 style={{ color: "#000000", textAlign: "center" }}>Mapa</h1>
+    <MapContainer
+      style={{ height: "calc(100vh - 56px)" }}
+      center={[-8.52795743904315, -55.91004811864883]}
+      zoom={18}
+      scrollWheelZoom
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker />
+    </MapContainer>
   );
 }
