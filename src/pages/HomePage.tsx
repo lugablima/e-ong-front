31a3f4330f9176132249d35db37ec3bf1c10d637
Lucas <lugablima@gmail.com>
2022-10-09 09:48:55 -0300

    feat: adds the location of all NGOs on the homepage map

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
-function LocationMarker() {
+function LocationMarkerUser() {
   const map = useMap();
   const [latitude, setLatitude] = useState<number>(0);
   const [longitude, setLongitude] = useState<number>(0);
@@ -25,14 +25,43 @@ function LocationMarker() {
 
   return (
     <Marker position={[latitude, longitude]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
+      <Popup>Esta é a sua localização</Popup>
     </Marker>
   );
 }
 
+interface LocationMarkerNgoProps {
+  lat: number;
+  lon: number;
+}
+
+function LocationMarkerNgo({ lat, lon }: LocationMarkerNgoProps) {
+  return (
+    <Marker position={[lat, lon]}>
+      <Popup>Esta é uma ONG!</Popup>
+    </Marker>
+  );
+}
+
+interface INgo {
+  type: string;
+  id: number;
+  lat: number;
+  lon: number;
+  tags: object;
+}
+
 export default function HomePage() {
+  const [ngo, setNgo] = useState<INgo[] | null>(null);
+
+  useEffect(() => {
+    fetch("./ngo.json", {
+      headers: { Accept: "application/json" },
+    })
+      .then((res) => res.json())
+      .then((res) => setNgo(res.elements));
+  }, []);
+
   return (
     <MapContainer
       style={{ height: "calc(100vh - 56px)", marginTop: "56px" }}
@@ -44,7 +73,12 @@ export default function HomePage() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LocationMarker />
+      <LocationMarkerUser />
+      {!ngo
+        ? ""
+        : ngo.map((el) => (
+            <LocationMarkerNgo key={el.id} lat={el.lat} lon={el.lon} />
+          ))}
     </MapContainer>
   );
 }
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,9 +1,28 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function ProfilePage() {
   return (
-    <Text fontFamily="default" fontSize={100} fontWeight={700}>
-      Eu sou a profile page!
-    </Text>
+    <Flex
+      direction="column"
+      justify="flex-start"
+      align="center"
+      rowGap="15px"
+      p="119px 0 67px"
+      overflowY="auto"
+    >
+      <Text
+        fontFamily="default"
+        color="secondary"
+        fontWeight="600"
+        fontSize="30px"
+        textAlign="center"
+        mb="15px"
+      >
+        Perfil
+      </Text>
+      {/* <FormLabel>
+      Nome
+    </FormLabel>   */}
+    </Flex>
   );
 }
